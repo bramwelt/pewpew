f63b2072886590984103a4aebe3e2d35e7bcb318
Trevor Bramwell <trevor@bramwell.net>
2014-06-20 23:17:47 -0700

    Fix enemy positioning.
    Preset offsets for x and y coordinates.
    Mothership commented out for the time being.

--- js/enemies.js
+++ js/enemies.js
@@ -4,6 +4,8 @@
 
 PewPew.Enemies = function(game) {
   Phaser.Group.call(this, game);
+  this.OFFSETX = (this.game.width/4);
+  this.OFFSETY = 64;
 }
 
 PewPew.Enemies.prototype = Object.create(Phaser.Group.prototype);
@@ -11,31 +13,32 @@ PewPew.Enemies.prototype.constructor = PewPew.Enemies;
 
 PewPew.Enemies.prototype.initialize = function() {
   for (var x = 0; x < 11; x++) {
-      var enemy = this.create(2+(x*18), 0, 'enemy1');
+      var enemy = this.create(this.OFFSETX+(x*18), this.OFFSETY, 'enemy1');
+      enemy.anchor.setTo(0.5, 0.5);
       enemy.animations.add('fly');
       enemy.animations.play('fly', 1, true);
   }
   for (var x = 0; x < 11; x++) {
       for (var y = 1; y < 3; y++) {
-          var enemy2 = this.create(x*18, y*16, 'enemy2');
+          var enemy2 = this.create(this.OFFSETX+(x*18), this.OFFSETY+(y*16), 'enemy2');
+          enemy2.anchor.setTo(0.5, 0.5);
           enemy2.animations.add('fly');
           enemy2.animations.play('fly', 1, true);
       }
   }
   for (var x = 0; x < 11; x++) {
       for (var y = 1; y < 3; y++) {
-          var enemy3 = this.create(x*18, 32+(y*16), 'enemy3');
+          var enemy3 = this.create(this.OFFSETX+(x*18), this.OFFSETY+32+(y*16), 'enemy3');
+          enemy3.anchor.setTo(0.5, 0.5);
           enemy3.animations.add('fly');
           enemy3.animations.play('fly', 1, true);
       }
   }
-  this.position.x = this.game.world.centerX/2;
-  this.position.y = (this.game.world.centerX/2)-64;
 
   this.game.physics.arcade.enable(this);
 
-  mothership = this.create(this.midx+64, 0, 'mothership');
-  mothership.animations.add('fly');
-  mothership.animations.play('fly', 1, true);
-  mothership.kill();
+  //mothership = this.create(this.midx+64, 0, 'mothership');
+  //mothership.animations.add('fly');
+  //mothership.animations.play('fly', 1, true);
+  //mothership.kill();
 };
