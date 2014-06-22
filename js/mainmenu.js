aa9b4676c9eb82ef2cd6e30f19046ec15fbe1878
Trevor Bramwell <trevor@bramwell.net>
2014-06-21 17:36:47 -0700

    Add Title to mainmenu, and require interaction.

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -16,6 +16,17 @@ PewPew.MainMenu.prototype = {
   },
 
   create: function() {
+    var style = {
+        font:'16px Lucida Console, Monaco, monospace',
+        fill: '#ffffff',
+        align: 'center',
+    };
+    var title = this.game.add.text(this.game.world.centerX, 8, 'PewPew: Now with more lasers!', style);
+    title.anchor.setTo(0.5, 0);
+    this.input.onDown.addOnce(this.begin, this);
+  },
+
+  begin: function(pointer) {
     /* Later on we will render text and sprites here, along with a click
      * handler for starting the game state.*/
     this.state.start('game');
